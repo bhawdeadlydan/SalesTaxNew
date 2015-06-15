a3774dfc52881fd6bc654203df33a7e78aca08f3
Bhawesh Kumar Singh <bhawdeadlydan@gmail.com>
2015-06-15 11:30:38 +0530

    Added Spec for parsing user Input

--- test/com/pathshala/Sales/ParserTest.java
+++ test/com/pathshala/Sales/ParserTest.java
@@ -10,5 +10,19 @@ import static org.junit.Assert.assertEquals;
 
 public class ParserTest {
 
-    
+    @Test
+    public void shouldParseUserInput() {
+        View view = new View(new Scanner(System.in));
+        HashMap<String, String[]> masterList = new HashMap<String, String[]>();
+        Parser parser = new Parser(view, masterList);
+
+
+        Item actualItem = parser.parse("1 book at 12.79");
+
+        Item expectedItem = new Item(12.79, false, 1, "book", true);
+
+        assertEquals(actualItem, is(expectedItem));
+
+    }
+
 }
